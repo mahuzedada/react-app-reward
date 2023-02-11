635b2566c933fadf8b46f0a92eba9b76f4fc6947
Chatis Santos <chatis@afrointelligence.com>
2023-02-11 15:33:01 -0600

    added more tests for reward computation

--- src/helpers/computeRewards.test.js
+++ src/helpers/computeRewards.test.js
@@ -12,3 +12,9 @@ test('should compute for one transaction between 50 and 100', () => {
 test('should compute for one transaction less than 50', () => {
   expect(computeRewards([{ amount: 49.99999999 }])).toEqual(0);
 });
+test('should compute for one transaction equal to 50', () => {
+  expect(computeRewards([{ amount: 50 }])).toEqual(0);
+});
+test('should compute for one transaction equal to 100', () => {
+  expect(computeRewards([{ amount: 100 }])).toEqual(50);
+});
